3350535f60dd660d0725fe66f50284e28ba2bec5
Shivambhagatdev <bhagatshivam551@gmail.com>
2026-09-15 23:34:43 +0530

    Time: 25 ms (81.68%), Space: 86.8 MB (9.9%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+         mergeSort(nums, 0, nums.length - 1);
+
+        return nums;  
+    }
+   public void mergeSort(int[] nums,int st,int end){
+    if(st>=end){
+        return ;
+    }
+    int mid=(st+end)/2;
+    mergeSort(nums,st,mid);
+    mergeSort(nums,mid+1,end);
+    merge(nums,st,mid,end);
+   } 
+   public void merge(int[] nums,int st,int mid,int end){
+    int i=st;
+    int j=mid+1;
+    int k=0;
+    int[] temp=new int[end-st+1];
+    while(i<=mid&&j<=end){
+        if(nums[i]<nums[j]){
+            temp[k]=nums[i];
+            i++;
+        }else{
+            temp[k]=nums[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<=mid){
+        temp[k]=nums[i];
+        i++;k++;
+    }
+    while(j<=end){
+        temp[k]=nums[j];
+        j++;k++;
+    }
+    for(int x = 0; x < temp.length; x++) {
+            nums[st + x] = temp[x];
+        }
+   }
+}

8641afe72f6bf90359315d7c77a7486d82d9e78d
Shivambhagatdev <bhagatshivam551@gmail.com>
2026-09-18 22:45:54 +0530

    Time: 0 ms (100%), Space: 52.2 MB (5.58%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,51 +1,35 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-
-        int n = matrix.length;
-
-        int low = matrix[0][0];
-        int high = matrix[n - 1][n - 1];
-
-        while (low < high) {
-
-            int mid = low + (high - low) / 2;
-
-            int count = countLessEqual(matrix, mid);
-
-            if (count < k) {
-                low = mid + 1;
-            } else {
-                high = mid;
+        int n=matrix.length;
+        int lo=matrix[0][0];
+        int hi=matrix[n-1][n-1];
+        while(lo<hi){
+          int mid = lo + (hi - lo) / 2;
+            int count=Countlessthan(matrix,mid);
+            if(count<k){
+                lo=mid+1;
+            }else{
+                hi=mid;
             }
         }
-
-        return low;
+         return lo;
     }
-
-    public int countLessEqual(int[][] matrix, int target) {
-
-        int count = 0;
-
-        for (int[] row : matrix) {
-
-            int left = 0;
-            int right = row.length;
-
-            // Upper Bound: first element > target
-            while (left < right) {
-
-                int mid = left + (right - left) / 2;
-
-                if (row[mid] <= target) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
-
-            count += left;
+    public int Countlessthan(int[][] matrix,int target){
+        int count=0;
+        for(int[] row:matrix){
+            int left=0;
+            int right=row.length;
+            while(left<right){
+             int mid = left + (right - left) / 2;
+               if(row[mid]<=target){
+                left=mid+1;
+               }else{
+                right=mid;
+               } 
+               } 
+                count+=left;          
         }
-
-        return count;
+       return count;
     }
+
 }
